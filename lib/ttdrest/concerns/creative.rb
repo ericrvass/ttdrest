c62c88e75dafc3a3e3a7ed368046b2c6f4cc7aea
Eric Vass <ericrvass@gmail.com>
2014-10-17 11:05:51 -0400

    Add update for ad creative

--- lib/ttdrest/concerns/creative.rb
+++ lib/ttdrest/concerns/creative.rb
@@ -50,6 +50,40 @@ module Ttdrest
         result = data_post(path, content_type, creative_data.to_json)
         return result
       end
+
+      def update_image_creative(creative_id, name, image_content, clickthrough_url, landing_page_url, options = {})
+        advertiser_id = self.advertiser_id || options[:advertiser_id]
+        path = "/creative"
+        content_type = 'application/json'
+        creative_data = {
+          "AdvertiserId" => advertiser_id,
+          "CreativeName" => name
+          }
+        params = options[:params] || {}
+        if !params[:description].nil?
+          creative_data = creative_data.merge({"Description" => params[:description]})
+        end
+        if !params[:third_party_impression_tracking_url].nil?
+          creative_data = creative_data.merge({"ThirdPartyImpressionTrackingUrl" => params[:third_party_impression_tracking_url]})
+        end
+        if !params[:right_media_offer_type_id].nil?
+          creative_data = creative_data.merge({"RightMediaOfferTypeId" => params[:right_media_offer_type_id]})
+        end
+        if !params[:ad_technology_ids].nil?
+          creative_data = creative_data.merge({"AdTechnologyIds" => params[:ad_technology_ids]})
+        end
+        
+        image_data = {
+          "ImageContent" => image_content,
+          "ClickthroughUrl" => clickthrough_url,
+          "LandingPageUrl" => landing_page_url
+          }
+        
+        creative_data = creative_data.merge({"ImageAttributes" => image_data})
+        
+        result = data_put(path, content_type, creative_data.to_json)
+        return result
+      end
       
       #TODO: Facebook Creatives
       
