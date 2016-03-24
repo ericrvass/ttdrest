7043904b3c9da888d36500a85e9afa7d68556191
stevievines <stevievines@gmail.com>
2016-03-24 16:02:42 -0400

    Update Creative to V3 Compatibility

--- lib/ttdrest/concerns/creative.rb
+++ lib/ttdrest/concerns/creative.rb
@@ -4,19 +4,19 @@ module Ttdrest
 
       def get_creatives(options = {})
         advertiser_id = self.advertiser_id || options[:advertiser_id]
-        path = "/creatives/#{advertiser_id}"
-        params = {}
-        result = get(path, params)
-        return result
+        path = "/creative/query/advertiser"
+        params = { AdvertiserId: advertiser_id, PageStartIndex: 0, PageSize: nil }
+        content_type = 'application/json'
+        data_post(path, content_type, params.to_json)
       end
-      
+
       def get_creative(creative_id, options = {})
         path = "/creative/#{creative_id}"
         params = {}
         result = get(path, params)
         return result
       end
-      
+
       def create_image_creative(name, image_content, clickthrough_url, landing_page_url, options = {})
         advertiser_id = self.advertiser_id || options[:advertiser_id]
         path = "/creative"
@@ -24,29 +24,28 @@ module Ttdrest
         creative_data = {
           "AdvertiserId" => advertiser_id,
           "CreativeName" => name
-          }
+        }
         params = options[:params] || {}
         if !params[:description].nil?
           creative_data = creative_data.merge({"Description" => params[:description]})
         end
-        if !params[:third_party_impression_tracking_url].nil?
-          creative_data = creative_data.merge({"ThirdPartyImpressionTrackingUrl" => params[:third_party_impression_tracking_url]})
-        end
-        if !params[:right_media_offer_type_id].nil?
-          creative_data = creative_data.merge({"RightMediaOfferTypeId" => params[:right_media_offer_type_id]})
-        end
-        if !params[:ad_technology_ids].nil?
-          creative_data = creative_data.merge({"AdTechnologyIds" => params[:ad_technology_ids]})
-        end
-        
+
         image_data = {
           "ImageContent" => image_content,
           "ClickthroughUrl" => clickthrough_url,
           "LandingPageUrl" => landing_page_url
-          }
-        
+        }
+
+        if !params[:right_media_offer_type_id].nil?
+          image_data = image_data.merge({"RightMediaOfferTypeId" => params[:right_media_offer_type_id]})
+        end
+
+        if !params[:ad_technology_ids].nil?
+          image_data = image_data.merge({"AdTechnologyIds" => params[:ad_technology_ids]})
+        end
+
         creative_data = creative_data.merge({"ImageAttributes" => image_data})
-        
+
         result = data_post(path, content_type, creative_data.to_json)
         return result
       end
@@ -59,36 +58,33 @@ module Ttdrest
           "CreativeId" => creative_id,
           "AdvertiserId" => advertiser_id,
           "CreativeName" => name
-          }
+        }
         params = options[:params] || {}
         if !params[:description].nil?
           creative_data = creative_data.merge({"Description" => params[:description]})
         end
-        if !params[:third_party_impression_tracking_url].nil?
-          creative_data = creative_data.merge({"ThirdPartyImpressionTrackingUrl" => params[:third_party_impression_tracking_url]})
-        end
-        if !params[:right_media_offer_type_id].nil?
-          creative_data = creative_data.merge({"RightMediaOfferTypeId" => params[:right_media_offer_type_id]})
-        end
-        if !params[:ad_technology_ids].nil?
-          creative_data = creative_data.merge({"AdTechnologyIds" => params[:ad_technology_ids]})
-        end
-        
+
         image_data = {
           "ClickthroughUrl" => clickthrough_url,
           "LandingPageUrl" => landing_page_url
-          }
+        }
 
-        if !image_data.blank?
+        if !params[:right_media_offer_type_id].nil?
+          image_data = image_data.merge({"RightMediaOfferTypeId" => params[:right_media_offer_type_id]})
+        end
+        if !params[:ad_technology_ids].nil?
+          image_data = image_data.merge({"AdTechnologyIds" => params[:ad_technology_ids]})
+        end
+        if !image_content.blank?
           image_data = image_data.merge({"ImageContent" => image_content})
         end
-        
+
         creative_data = creative_data.merge({"ImageAttributes" => image_data})
-        
+
         result = data_put(path, content_type, creative_data.to_json)
         return result
       end
-      
+
       def create_facebook_creative(name, image_content, clickthrough_url, landing_page_url, title, description, options = {})
         advertiser_id = self.advertiser_id || options[:advertiser_id]
         path = "/creative"
@@ -96,31 +92,29 @@ module Ttdrest
         creative_data = {
           "AdvertiserId" => advertiser_id,
           "CreativeName" => name
-          }
+        }
         params = options[:params] || {}
         if !params[:description].nil?
           creative_data = creative_data.merge({"Description" => params[:description]})
         end
-        if !params[:third_party_impression_tracking_url].nil?
-          creative_data = creative_data.merge({"ThirdPartyImpressionTrackingUrl" => params[:third_party_impression_tracking_url]})
-        end
-        if !params[:right_media_offer_type_id].nil?
-          creative_data = creative_data.merge({"RightMediaOfferTypeId" => params[:right_media_offer_type_id]})
-        end
-        if !params[:ad_technology_ids].nil?
-          creative_data = creative_data.merge({"AdTechnologyIds" => params[:ad_technology_ids]})
-        end
-        
+
         image_data = {
           "ImageContent" => image_content,
           "ClickthroughUrl" => clickthrough_url,
           "LandingPageUrl" => landing_page_url,
           "FacebookTitle" => title,
           "FacebookBody" => description
-          }
-        
+        }
+
+        if !params[:right_media_offer_type_id].nil?
+          image_data = image_data.merge({"RightMediaOfferTypeId" => params[:right_media_offer_type_id]})
+        end
+        if !params[:ad_technology_ids].nil?
+          image_data = image_data.merge({"AdTechnologyIds" => params[:ad_technology_ids]})
+        end
+
         creative_data = creative_data.merge({"FacebookAttributes" => image_data})
-        
+
         result = data_post(path, content_type, creative_data.to_json)
         return result
       end
@@ -133,34 +127,32 @@ module Ttdrest
           "CreativeId" => creative_id,
           "AdvertiserId" => advertiser_id,
           "CreativeName" => name
-          }
+        }
         params = options[:params] || {}
         if !params[:description].nil?
           creative_data = creative_data.merge({"Description" => params[:description]})
         end
-        if !params[:third_party_impression_tracking_url].nil?
-          creative_data = creative_data.merge({"ThirdPartyImpressionTrackingUrl" => params[:third_party_impression_tracking_url]})
-        end
-        if !params[:right_media_offer_type_id].nil?
-          creative_data = creative_data.merge({"RightMediaOfferTypeId" => params[:right_media_offer_type_id]})
-        end
-        if !params[:ad_technology_ids].nil?
-          creative_data = creative_data.merge({"AdTechnologyIds" => params[:ad_technology_ids]})
-        end
-        
+
         image_data = {
           "ClickthroughUrl" => clickthrough_url,
           "LandingPageUrl" => landing_page_url,
           "FacebookTitle" => title,
           "FacebookBody" => description
-          }
+        }
 
-        if !image_data.blank?
+        if !params[:right_media_offer_type_id].nil?
+          image_data = image_data.merge({"RightMediaOfferTypeId" => params[:right_media_offer_type_id]})
+        end
+        if !params[:ad_technology_ids].nil?
+          image_data = image_data.merge({"AdTechnologyIds" => params[:ad_technology_ids]})
+        end
+
+        if !image_content.blank?
           image_data = image_data.merge({"ImageContent" => image_content})
         end
-        
+
         creative_data = creative_data.merge({"FacebookAttributes" => image_data})
-        
+
         result = data_put(path, content_type, creative_data.to_json)
         return result
       end
@@ -174,9 +166,6 @@ module Ttdrest
           "CreativeName" => name
         }
         params = options[:params] || {}
-        if !params[:right_media_offer_type_id].nil?
-          creative_data = creative_data.merge({"RightMediaOfferTypeId" => params[:right_media_offer_type_id]})
-        end
 
         flash_data = {
           "FlashContent" => flash_content,
@@ -185,11 +174,15 @@ module Ttdrest
           "FlashClickTrackingParameterName" => params[:click_tag].nil? ? "clickTAG" : params[:click_tag]
         }
 
+        if !params[:right_media_offer_type_id].nil?
+          flash_data = flash_data.merge({"RightMediaOfferTypeId" => params[:right_media_offer_type_id]})
+        end
+
         creative_data = creative_data.merge({"FlashAttributes" => flash_data})
         result = data_post(path, content_type, creative_data.to_json)
         return result
       end
-      
+
       #TODO: 3rd Party HTML Creatives
 
       def create_video_creative(name, video_content, clickthrough_url, landing_page_url, options = {})
@@ -199,26 +192,24 @@ module Ttdrest
         creative_data = {
           "AdvertiserId" => advertiser_id,
           "CreativeName" => name
-          }
+        }
         params = options[:params] || {}
         if !params[:description].nil?
           creative_data = creative_data.merge({"Description" => params[:description]})
-        end
-        if !params[:third_party_impression_tracking_url].nil?
-          creative_data = creative_data.merge({"ThirdPartyImpressionTrackingUrl" => params[:third_party_impression_tracking_url]})
-        end
-        if !params[:right_media_offer_type_id].nil?
-          creative_data = creative_data.merge({"RightMediaOfferTypeId" => params[:right_media_offer_type_id]})
-        end
-        if !params[:ad_technology_ids].nil?
-          creative_data = creative_data.merge({"AdTechnologyIds" => params[:ad_technology_ids]})
         end
 
         video_data = {
           "VideoContent" => video_content,
           "ClickthroughUrl" => clickthrough_url,
           "LandingPageUrl" => landing_page_url
-          }
+        }
+
+        if !params[:right_media_offer_type_id].nil?
+          video_data = video_data.merge({"RightMediaOfferTypeId" => params[:right_media_offer_type_id]})
+        end
+        if !params[:ad_technology_ids].nil?
+          video_data = video_data.merge({"AdTechnologyIds" => params[:ad_technology_ids]})
+        end
 
         creative_data = creative_data.merge({"TradeDeskHostedVideoAttributes" => video_data})
 
