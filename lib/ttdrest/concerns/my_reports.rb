5d14b5063fa5a63b8437c666472d22612d2f6dfb
Eric Vass <ericrvass@gmail.com>
2016-09-26 15:22:14 -0400

    Add ability to set my reports execution start/end dates and page

--- lib/ttdrest/concerns/my_reports.rb
+++ lib/ttdrest/concerns/my_reports.rb
@@ -8,9 +8,9 @@ module Ttdrest
         content_type = 'application/json'
         report_data = {
           "AdvertiserIds" => [advertiser_id],
-          "ExecutionSpansStartDate" => report_date.strftime("%Y-%m-%dT00:00:00.0000000+00:00"),
-          "ExecutionSpansEndDate" => (report_date + 1.day).strftime("%Y-%m-%dT00:00:00.0000000+00:00"),
-          "PageStartIndex" => 0,
+          "ExecutionSpansStartDate" => options[:execution_spans_start_date].present? ? options[:execution_spans_start_date] : report_date.strftime("%Y-%m-%dT00:00:00.0000000+00:00"),
+          "ExecutionSpansEndDate" => options[:execution_spans_end_date].present? ? options[:execution_spans_end_date] : (report_date + 1.day).strftime("%Y-%m-%dT00:00:00.0000000+00:00"),
+          "PageStartIndex" => options[:page].present? ? options[:page] : 0,
           "PageSize" => 1,
           "ExecutionStates" => options[:execution_states].present? ? options[:execution_states] : ["Complete"]
         }
