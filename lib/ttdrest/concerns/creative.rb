01b9f535d80a7deeb5050a11ddf10f58975c1712
Brian Malinconico <bmalinconico@terminus.com>
2017-01-25 14:38:36 -0500

    Removing pry

--- lib/ttdrest/concerns/creative.rb
+++ lib/ttdrest/concerns/creative.rb
@@ -28,7 +28,6 @@ module Ttdrest
       end
 
       def update_creative(creative_data:)
-        binding.pry
         data_put(
           path = "/creative",
           content_type = 'application/json',
