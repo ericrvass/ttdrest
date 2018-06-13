bd57cfa21e32e7aa6729d65cacdc80f268217e72
Brian Malinconico <brian.malinconico@terminus.com>
2018-06-13 10:39:03 -0400

    Adding instrumentation hooks for request and resonses (#7)
    * Adding instrumentation hooks for request and resonses
    
    * Fixing logic
    
    * Fixing typo
    
    * Adding socket error as retryable error
    
    * Adding specs for new code

--- lib/ttdrest/concerns/base.rb
+++ lib/ttdrest/concerns/base.rb
@@ -1,5 +1,15 @@
+require 'date'
+require 'active_support/notifications'
+
 module Ttdrest
   class AuthorizationFailedError < StandardError; end
+  class RecoverableHttpError < StandardError
+    def initialize(message = nil, data = {})
+      super(message)
+
+      @data = data
+    end
+  end
 
   module Concerns
     module Base
@@ -8,6 +18,8 @@ module Ttdrest
 
       VERSION = "v3"
       RETRIES = 2
+      ERROR_RETRIES = 3
+      MAXIMUM_WAIT_TIME = 120
 
       def authenticate(options = {})
         client_id = self.client_login || options[:client_login]
@@ -20,7 +32,60 @@ module Ttdrest
       def check_response(response)
         if response.code.eql?("403")
           raise AuthorizationFailedError
+        elsif retryable_http_error?(response)
+          raise RecoverableHttpError.new('', response)
         end
+      end
+
+      def retryable_http_error?(response)
+        response && (
+          response.code == 429 || (500..599).include?(response.code)
+        )
+      end
+
+      def parse_header_retry(response)
+        return nil unless response && response['Retry-After']
+
+        if response['Retry-After'] =~ /^\d+$/
+          response['Retry-After'].to_i
+        else
+          date = Time.rfc2822(response['Retry-After'])
+          wait_time = date.to_i - Time.now.to_i
+          return nil unless wait_time.positive?
+          wait_time > MAXIMUM_WAIT_TIME ? MAXIMUM_WAIT_TIME :  wait_time
+        end
+      rescue ArgumentError
+        nil
+      end
+
+      def perform_request(request, connection = http_connection)
+        retries = 0
+        response = nil
+
+        begin
+          ActiveSupport::Notifications.instrument('ttd.request') do |payload|
+            payload[:request] = request
+            payload[:retries] = retries
+
+            response = connection.request(request)
+            payload[:response] = response
+          end
+
+          check_response(response)
+        rescue RecoverableHttpError, SocketError
+          raise if retries >= ERROR_RETRIES
+          retries += 1
+
+          if sleep_time = parse_header_retry(response)
+            sleep(sleep_time)
+          else
+            sleep((1.25 ** retries) * 1 - (0.3 * rand))
+          end
+
+          retry
+        end
+
+        response
       end
 
       def get(path, params)
@@ -28,8 +93,7 @@ module Ttdrest
         begin
           request = Net::HTTP::Get.new(params.blank? ? "/#{VERSION}#{path}" : "/#{VERSION}#{path}?".concat(params.collect { |k,v| "#{k}=#{CGI::escape(v.to_s)}" }.join('&')))
           request['TTD-Auth'] = self.auth_token
-          response = http_connection.request(request)
-          check_response(response)
+          response = perform_request(request)
           result = response.body.blank? ? "" : JSON.parse(response.body)
           return result
         rescue AuthorizationFailedError
@@ -49,8 +113,7 @@ module Ttdrest
           request = Net::HTTP::Post.new("/#{VERSION}#{path}", initheader = {'Content-Type' => content_type})
           request['TTD-Auth'] = self.auth_token
           request.body = json_data
-          response = http_connection.request(request)
-          check_response(response)
+          response = perform_request(request)
           result = response.body.blank? ? "" : JSON.parse(response.body)
           return result
         rescue AuthorizationFailedError
@@ -70,8 +133,7 @@ module Ttdrest
           request = Net::HTTP::Put.new("/#{VERSION}#{path}", initheader = {'Content-Type' => content_type})
           request['TTD-Auth'] = self.auth_token
           request.body = json_data
-          response = http_connection.request(request)
-          check_response(response)
+          response = perform_request(request)
           result = response.body.blank? ? "" : JSON.parse(response.body)
           return result
         rescue AuthorizationFailedError
---
+++ spec/ttdrest/concerns/base_spec.rb
@@ -0,0 +1,106 @@
+require 'time'
+require 'spec_helper'
+
+RSpec.describe 'Base Concern' do
+  let(:client) { Ttdrest::Client.new }
+
+  let(:faux_response) { Struct.new(:code, keyword_init: true) }
+
+  describe 'parse_header_retry' do
+    it 'returns nil if the objects lacks Retry-After' do
+      expect(client.parse_header_retry({})).to be_nil
+    end
+
+    it 'parses numeric waits' do
+      expect(client.parse_header_retry({'Retry-After' => '1234'})).to eq 1234
+    end
+
+    it 'parses date strings' do
+      date = Time.now + 60
+
+      expect(
+        client.parse_header_retry({'Retry-After' => date.rfc2822})
+      ).to be_within(1).of(60)
+    end
+
+    it 'returns nil for invalid dates' do
+      date_string = 'Wed, 41 Oct 3015 07:28:00 GMT'
+
+      expect(
+        client.parse_header_retry({'Retry-After' => date_string})
+      ).to be_nil
+    end
+
+    it 'returns nil if the date is in the past' do
+      date_string = (Time.now - 60).rfc2822
+
+      expect(
+        client.parse_header_retry({'Retry-After' => date_string})
+      ).to be_nil
+    end
+
+    it 'returns a maximum of 120seconds' do
+      date_string = (Time.now + 200).rfc2822
+
+      expect(
+        client.parse_header_retry({'Retry-After' => date_string})
+      ).to eq 120
+    end
+  end
+
+  describe '#retryable_http_error?' do
+    it 'is false when passed nil' do
+      expect(client.retryable_http_error?(nil)).to be_falsy
+    end
+
+    it 'is true when the http code is 429' do
+      expect(client.retryable_http_error?(faux_response.new(code: 429))).to be_truthy
+    end
+
+    it 'is true when in the 500 range' do
+      expect(client.retryable_http_error?(faux_response.new(code: 501))).to be_truthy
+    end
+
+    it 'is false when the http code is 404' do
+      expect(client.retryable_http_error?(faux_response.new(code: 404))).to be_falsy
+    end
+  end
+
+  describe '#perform_request' do
+    let(:connection) { double() }
+    let(:request) { double() }
+    let(:code) { 200 }
+
+    before do
+      allow(connection).to receive(:request).and_return(faux_response.new(code: code))
+      allow(client).to receive(:sleep)
+    end
+
+    it 'backs off 3 times with jitter' do
+      allow(connection).to receive(:request).and_raise(Ttdrest::RecoverableHttpError.new)
+      expect(client).to receive(:sleep).with(be_within(0.375).of(1.25 ** 1))
+      expect(client).to receive(:sleep).with(be_within(0.469).of(1.25 ** 2))
+      expect(client).to receive(:sleep).with(be_within(0.586).of(1.25 ** 3))
+
+      expect(connection).to receive(:request).with(request).exactly(4).times
+
+      expect { client.perform_request(request, connection) }.to raise_error(Ttdrest::RecoverableHttpError)
+    end
+
+    it 'respects the retry-after time if detected' do
+      allow(connection).to receive(:request).and_raise(Ttdrest::RecoverableHttpError.new)
+      allow(client).to receive(:parse_header_retry).and_return(12)
+      expect(client).to receive(:sleep).with(12).exactly(3).times
+
+      expect { client.perform_request(request, connection) }.to raise_error(Ttdrest::RecoverableHttpError)
+    end
+
+    it 'returns the response when successful' do
+      response = faux_response.new(code: 200)
+
+      expect(connection).to receive(:request).with(request).and_return(response)
+      expect(client.perform_request(request, connection)).to eq response
+    end
+  end
+
+end
