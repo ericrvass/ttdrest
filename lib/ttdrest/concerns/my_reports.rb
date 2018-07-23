7a42648fb677b322029028aacc2fb0a0b49c19f7
Stephen Reid <stephenreid@ratedsar.com>
2018-07-23 10:24:54 -0500

    Missed variable. (#10)

--- lib/ttdrest/concerns/my_reports.rb
+++ lib/ttdrest/concerns/my_reports.rb
@@ -16,7 +16,7 @@ module Ttdrest
           search_params['ReportScheduleNameContains'] = options[:report_schedule_name_contains] if options[:report_schedule_name_contains]
           search_params['ExecutionStates'] = present_or_default(options[:execution_states],["Complete"])
           search_params['ExecutionSpansStartDate'] = present_or_default(options[:execution_spans_start_date], report_date.strftime("%Y-%m-%dT00:00:00.0000000+00:00"))
-          search_params['ExecutionSpansEndDate'] = present_or_default([:execution_spans_end_date], (report_date + 1.day).strftime("%Y-%m-%dT00:00:00.0000000+00:00"))
+          search_params['ExecutionSpansEndDate'] = present_or_default(options[:execution_spans_end_date], (report_date + 1.day).strftime("%Y-%m-%dT00:00:00.0000000+00:00"))
           search_params['PageStartIndex'] = present_or_default(options[:page], 0)
           search_params['PageSize'] = present_or_default(options[:page_size],1)
           search_params['SortFields'] = options[:sort_fields] if options[:sort_fields]
